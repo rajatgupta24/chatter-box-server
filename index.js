188af834abe613079f8261c44892a11099a2a385
rajat <rajat2411gupta@gmail.com>
2021-05-07 06:58:16 +0530

    link established

--- index.js
+++ index.js
@@ -9,12 +9,29 @@ const io = require("socket.io")(httpServer, {
 
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
+const { addUser, removeUser, getAdd, getUsersInRoom, getUser } = require("./users");
 
 io.on("connection", (socket) => {
   console.log("we are connected to server!!!");
 
-  socket.on("join", ({ name, room }) => {
-    console.log(name, room);
+
+  socket.on("join", ({ name, room }, callback) => {
+    const {user, error} = addUser({ id: socket.id, name, room });
+    
+    if (error) return callback(error);
+
+    socket.emit("message", {user: "admin", text:`${user.name} welcome to the room`});
+    socket.broadcast.to(user.room).emit("message", { user: "admin", text:`${user.name} has joined` });
+
+    socket.join(user.room);
+    callback()
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    console.log(user);
+    io.to(user.room).emit("message", { user: user.name, text: message});
+    console.log(message);
 
   });
 

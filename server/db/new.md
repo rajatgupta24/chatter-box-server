6aa5e18737675b60c905f94445f9a31f921403c5
rajatgupta24 <rajat2411gupta@gmail.com>
2022-04-29 23:33:42 +0530

    fix: node server to convert a md to pdf

---
+++ server/db/new.md
@@ -0,0 +1,17 @@
+ simple markdown editor with preview, implemented with React.js and TypeScript. This React Component aims to provide a simple Markdown editor with syntax highlighting support. This is based on `textarea` encapsulation, so it does not depend on any modern code editors such as Acs, CodeMirror, Monaco etc.
+
+### Features
+
+- 📑 Indent line or selected text by pressing tab key, with customizable indentation.
+- ♻️ Based on `textarea` encapsulation, does not depend on any modern code editors.
+- 🚧 Does not depend on the [`uiw`](https://github.com/uiwjs/uiw) component library.
+- 🚘 Automatic list on new lines.
+- 😻 GitHub flavored markdown support.
+- 🌒 Support dark-mode/night-mode **@v3.11.0+**.
+- 💡 Support [next.js](https://github.com/uiwjs/react-md-editor/issues/52#issuecomment-848969341), [Use examples](#support-nextjs) in [next.js](https://nextjs.org/).
+
+### Quick Start
+
+```bash
+npm i @uiw/react-md-editor
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+
+import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 import './App.css';
 
-const doc = new jsPDF();
+// const doc = new jsPDF();
 
 function App() {
   const [text, setText] = useState("")
 
   const save = () => {
-    doc.text(text, 10, 10);
-    doc.save("myDocument.pdf");
+    const input = document.getElementById("md") as HTMLElement;
+    
+    html2canvas(input, {
+      logging: true,
+      useCORS: true,
+    }).then(canvas => {
+      const imgWidth = 208;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const imgData = canvas.toDataURL("img/png")
+      const pdf = new jsPDF("p", "mm", "a4");
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      pdf.save("new.pdf");
+    })
   };
 
-  const saveMD = () => {
-    doc.text(text, 10, 10);
-    doc.save("myDocument.pdf");
-  };
+  const savePDF = () => {
+    
+  }
 
   return (
     <>
       <div className="App">
         <textarea className='editor' value={text} onChange={(e) => setText(e.target.value)}></textarea>
-        <div className='md'>
+        <div className='md' id="md">
           <ReactMarkdown children={text} />          
         </div>
       </div>
       <div className='btns'>
-        <button onClick={save}>Save PDF</button>
+        <button onClick={savePDF}>Save PDF</button>
         <button onClick={save}>Save MD</button>
       </div>
     </>
